50d94438708db6d985175f5f315933bb152930f2
junezuazo <71823613+junezuazo@users.noreply.github.com>
2023-05-03 14:25:07 +0200

    [PG][JZ] DIRECCIONES DE ENVIO USUARIO ACABADO

--- Practica4/includes/DAO/UserProductDAO.php
+++ Practica4/includes/DAO/UserProductDAO.php
@@ -23,7 +23,7 @@ class UserProductDAO extends DAO
     {
         $query = "SELECT * FROM users_products WHERE userID = :userID";
         $statement = $this->m_DatabaseProxy->prepare($query);
-        
+
         $statement->bindParam(':userID', $userID);
         $statement->execute();
 
@@ -33,24 +33,64 @@ class UserProductDAO extends DAO
         foreach ($statement as $result) {
             array_push($results, $userProductDAO->createDTOFromArray($result));
         }
-        
+
         return $results;
     }
 
     public function getCartProduct($userID, $productID): DTO
     {
-        $query = "SELECT u.amount FROM users_products u WHERE u.userID = :userID and u.productID = :productID" ;
+        $query = "SELECT u.amount FROM users_products u WHERE u.userID = :userID and u.productID = :productID";
         $statement = $this->m_DatabaseProxy->prepare($query);
-        
+
         $statement->bindParam(':userID', $userID);
         $statement->bindParam(':productID', $productID);
         $statement->execute();
 
         $amount = $statement->fetchColumn();
         $userProductDTO = new UserProductDTO($userID, $productID, $amount);
-        
-        
-        return $userProductDTO;   
+
+
+        return $userProductDTO;
+    }
+
+    
+
+    
+
+    public function updateWithCompoundKey($dto)
+    {
+        $dtoArray = $this->createArrayFromDTO($dto);
+        $dtoArrayKeys = array_keys($dtoArray);
+
+        $updateVariables = "{$dtoArrayKeys[0]} = :{$dtoArrayKeys[0]}";
+
+        for ($i = 1; $i < count($dtoArrayKeys); ++$i) {
+            $column = $dtoArrayKeys[$i];
+            $updateVariables .= ", $column = :$column";
+        }
+
+        $idKey1 = self::USER_ID_KEY;
+        $idKey2 = self::PRODUCT_ID_KEY;
+        $query = "UPDATE users_products SET $updateVariables WHERE $idKey1 = {$dto->getID1()} AND $idKey2 = {$dto->getID2()}";
+
+        $statement = $this->m_DatabaseProxy->prepare($query);
+
+        foreach ($dtoArrayKeys as $key) {
+            $statement->bindParam(":$key", $dtoArray[$key]);
+        }
+
+        return $statement->execute();
+    }
+
+    public function deleteProduct($userID, $productID): bool
+    {
+        $query = 'DELETE FROM users_products WHERE userID = :userID AND productID = :productID';
+
+        $statement = $this->m_DatabaseProxy->prepare($query);
+        $statement->bindParam(':userID', $userID);
+        $statement->bindParam(':productID', $productID);
+
+        return $statement->execute();
     }
 
     public function deleteCart(): bool
@@ -68,55 +108,25 @@ class UserProductDAO extends DAO
         
         $query = 'SELECT amount FROM users_products WHERE productID = :productID' ;
         $statement = $this->m_DatabaseProxy->prepare($query);
-
-    public function updateWithCompoundKey($dto){
-        $dtoArray = $this->createArrayFromDTO($dto);
-        $dtoArrayKeys = array_keys($dtoArray);
-        
-        $updateVariables = "{$dtoArrayKeys[0]} = :{$dtoArrayKeys[0]}";
-
-        for ($i = 1; $i < count($dtoArrayKeys); ++$i) {
-            $column = $dtoArrayKeys[$i];
-            $updateVariables .= ", $column = :$column";
-        }
-
-        $idKey1 = self::USER_ID_KEY;
-        $idKey2 = self::PRODUCT_ID_KEY;
-        $query = "UPDATE users_products SET $updateVariables WHERE $idKey1 = {$dto->getID1()} AND $idKey2 = {$dto->getID2()}";
-
-        $statement = $this->m_DatabaseProxy->prepare($query);
-
-        foreach ($dtoArrayKeys as $key) {
-            $statement->bindParam(":$key", $dtoArray[$key]);
-
-        }
-        
-        return $statement->execute();
-
-    }
-
-    public function deleteProduct($userID, $productID): bool
-    {
-        $query = 'DELETE FROM users_products WHERE userID = :userID AND productID = :productID';
-
-        $statement = $this->m_DatabaseProxy->prepare($query);
-        $statement->bindParam(':userID', $userID);
         $statement->bindParam(':productID', $productID);
 
         return $statement->execute();
     }
+  
+
+    
 
     protected function createDTOFromArray($array): DTO
     {
-        
+
         $userID = $array[self::USER_ID_KEY];
         $productID = $array[self::PRODUCT_ID_KEY];
         $amount = $array[self::AMOUNT_KEY];
-        
 
-        return new UserProductDTO($userID, $productID,$amount);
+
+        return new UserProductDTO($userID, $productID, $amount);
     }
-    
+
     protected function createArrayFromDTO($dto): array
     {
         $dtoArray = array(
@@ -127,8 +137,4 @@ class UserProductDAO extends DAO
 
         return $dtoArray;
     }
-
-    
-
 }
-?>
