3301edcd48fa63a2b18e8ba207759bb1521ebd3b
junezuazo <71823613+junezuazo@users.noreply.github.com>
2023-05-03 14:24:01 +0200

    [PG][JZ] DIRECCIONES DE ENVIO USUARIO ACABADO

--- Practica4/account.php
+++ Practica4/account.php
@@ -1,6 +1,8 @@
 <?php
 
 use es\ucm\fdi\aw\forms\AccountForm;
+use es\ucm\fdi\aw\DAO\AddressDAO;
+use es\ucm\fdi\aw\DAO\UserAddressDAO;
 
 require_once 'includes/config.php';
 
@@ -10,18 +12,84 @@ $title = 'Mi cuenta';
 <?php
 if (isset($_SESSION["user"])) {
     $userID = $_SESSION["user"]->getID();
+    $addressDAO = new AddressDAO();
+    $userAddressDAO = new UserAddressDAO();
+    $var = $addressDAO->getAddressForUser($userID);
+    $vID = 0;
 ?>
+
     <div class="container shadow p-4">
         <h1 class="mb-4 text-center">Datos del usuario</h1>
 
         <?= ($modUser = new AccountForm($userID))->handleForm(); ?>
+
+        <?php if($var != NULL) {
+             ?>
+
+        <table class="table">
+                <thead>
+                    <tr>
+                    <th></th>
+                    <th>Dirección</th>
+                    <th>Piso</th>
+                    <th>Código Postal</th>
+                    <th>Ciudad</th>
+                    <th>Provincia</th>
+                    <th>País</th>
+                    <th> </th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                    
+                    foreach ($var as $v) {
+                        echo "<tr>";
+                        echo "<td>" . $v['addressID'] . "</td>";
+                        echo "<td>" . $v['streetO'] . "</td>";
+                        echo "<td>" . $v['floorO'] . "</td>";
+                        echo "<td>" . $v['zipO'] . "</td>";
+                        echo "<td>" . $v['cityO'] . "</td>";
+                        echo "<td>" . $v['provinceO'] . "</td>";
+                        echo "<td>" . $v['countryO'] . "</td>";
+                        ?><form method="POST">
+                        <td> <button type="submit" name="delete" id="delete-{$v['addressID']}" class="py-1 btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#product-modal" value="<?php $v['addressID']?>">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"></path>
+                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"></path>
+                                </svg>
+                            </button>  </td></form>
+                        <?php
+                        echo "</tr>";
+                    }
+                    ?>
+                </tbody>
+                </table>
+                <?php } else echo "No hay direcciones de envío registradas.";
+                
+                ?> 
     </div>
+
+    
+
 <?php
 }
 
 ?>
 
-<?php
+<?php  
+
+if (isset($_POST['delete'])) {
+    $addressDAO->delete($v['addressID']);
+    //$userAddressDAO->deleteAddress($v['addressID']);
+    header('Location: account.php');
+    exit();
+}
+
+
+
+
+
 $content = ob_get_clean();
 
 
--- Practica4/purchase.php
+++ Practica4/purchase.php
@@ -5,6 +5,8 @@ use es\ucm\fdi\aw\DAO\UserOrderDAO;
 use es\ucm\fdi\aw\DAO\UserProductDAO;
 use es\ucm\fdi\aw\DAO\UserDAO;
 use es\ucm\fdi\aw\DAO\ProductDAO;
+use es\ucm\fdi\aw\DAO\UserAddressDAO;
+use es\ucm\fdi\aw\DAO\AddressDAO;
 
 require_once 'includes/config.php';
 
@@ -15,19 +17,23 @@ $prodDAO = new ProductDAO;
 $userProductDAO = new UserProductDAO;
 $productsPath = 'images/products/';
 $cartCount = 0;
+$addressDAO = new AddressDAO;
+$userAddressDAO = new UserAddressDAO;
 
+$uID = $_SESSION["user"]->getID();
+
+$addr = $addressDAO->read();
+
+$results = $addressDAO->getAddressForUser($uID);
 
 if (isset($_GET["subtotal"])){
     $subtotal = $_GET["subtotal"];
     $my_array = array();
-
-    $uID = $_SESSION["user"]->getID();
+   
     $my_array = $userProductDAO->getUserCart($uID);
 
     $cartCount = count($my_array);
-
-    
-    
+   
 
 
 } else {
@@ -36,7 +42,6 @@ if (isset($_GET["subtotal"])){
     $productDTOResults = $productDAO->read($productID);
     
     $product = $productDTOResults[0];
-    
     $url = "product.php?productID=" . $product->getID();
 }
 ob_start();
@@ -59,11 +64,37 @@ ob_start();
                     <!-- Sección de Entrega -->
                     <div style="width: 65%;">
                         <div class="border rounded p-3 mb-3">
-                            <h3><strong>Entrega</strong></h3>
-                            <label for="direccion"><strong>Dirección de envío: </strong> </label>
-                            <input type="text" id="direccion" name="direccion" value="<?php //echo $result['stateO']; ?>">
-                            <br>
-                            <br>
+                        <h3><strong>Entrega</strong></h3>
+                            <table class="table">
+                            <thead>
+                                <tr>
+                                <th></th>
+                                <th>Dirección</th>
+                                <th>Piso</th>
+                                <th>Código Postal</th>
+                                <th>Ciudad</th>
+                                <th>Provincia</th>
+                                <th>País</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                
+                                foreach ($results as $result) {
+                                    echo "<tr>";
+                                    echo "<td><input type='checkbox' name='direccion_envio' value='" . $result['addressID'] . "' data-id='" . $result['addressID'] . "' onclick='uncheckAll(this)'></td>";
+
+                                    echo "<td>" . $result['streetO'] . "</td>";
+                                    echo "<td>" . $result['floorO'] . "</td>";
+                                    echo "<td>" . $result['zipO'] . "</td>";
+                                    echo "<td>" . $result['cityO'] . "</td>";
+                                    echo "<td>" . $result['provinceO'] . "</td>";
+                                    echo "<td>" . $result['countryO'] . "</td>";
+                                    echo "</tr>";
+                                }
+                                ?>
+                            </tbody>
+                            </table>
                             <label for="fecha"><strong>Fecha de pedido:</strong>&nbsp;</label><?php echo date('Y-m-d'); ?>
                         </div>
                         <!-- Sección de Contacto -->
@@ -85,12 +116,14 @@ ob_start();
                         <!-- Sección de Pago -->
                         <div class="border rounded p-3 mb-3">
                             <h3><strong>Pago</strong></h3>
-                            <label for="metodo_pago"><strong>Método de pago: </strong> </label>
-                            <select name="metodo_pago" id="metodo_pago">
-                                <option value="Tarjeta Credito">Tarjeta de crédito</option>
-                                <option value="Transferencia Bancaria">Transferencia bancaria</option>
-                                <option value="Bizum">Bizum</option>
-                            </select>
+                            
+                                <label for="metodo_pago"><strong>Método de pago: </strong> </label>
+                                <select name="metodo_pago" id="metodo_pago">
+                                    <option value="Tarjeta Credito">Tarjeta de crédito</option>
+                                    <option value="Transferencia Bancaria">Transferencia bancaria</option>
+                                    <option value="Bizum">Bizum</option>
+                                </select>
+                            
                             <br>
                         </div>
                     </div>
@@ -99,7 +132,7 @@ ob_start();
                     <div style="width: 30%;">
                         <div class="border rounded p-3 mb-3">
                             <h3><strong>Resumen de pedido</strong></h3>
-                            <?php if (!isset($subtotal)) : ?>                                                              
+                            <?php if (!isset($subtotal)) : ?>
                                 <table>
                                     <tr>
                                         <td><strong>x1</strong></td>
@@ -133,16 +166,43 @@ ob_start();
                                 <?php else : echo $subtotal ?><?php endif; ?>
                                 €</p>
                             
-                            <button type="submit" name="buy" class="btn btn-success">Comprar</button>
+                            <!--<button id="buy-button" type="submit" name="buy" class="btn btn-success">Comprar</button>-->
+                            <form method="post">
+                                <input type="hidden" name="dir" id="dir-input">
+                                <!-- Agrega otros campos de entrada aquí -->
+                                <button type="submit" name="buy" class="btn btn-success" href="purchase.php?metodo=<?= $metodo ?>">Comprar</button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </form>
         </div>
 
+        <script>
+            function uncheckAll(clickedCheckbox) {
+                var checkboxes = document.getElementsByName("direccion_envio");
+                for (var i = 0; i < checkboxes.length; i++) {
+                if (checkboxes[i] !== clickedCheckbox) {
+                    checkboxes[i].checked = false;
+                }
+                }
+                            
+                // Obtener el ID del registro seleccionado
+                var dir = clickedCheckbox.value;
+                console.log("ID del registro seleccionado:", dir);
+                            
+                // Pasar el valor de dir al campo de entrada oculto
+                var dirInput = document.getElementById("dir-input");
+                dirInput.value = dir;
+            }
+        </script>
+
         <?php
+       
+       
         if (isset($_POST['buy'])) {
-            $dir = $_POST['direccion'];
+            $dir = $_POST["dir"];
+            //$dir = $_POST['direccion'];
             $nombre = $_POST['nombre'];
             $apellido = $_POST['apellido'];
             $new_email = $_POST['email'];
@@ -150,7 +210,11 @@ ob_start();
             $date = date('Y-m-d');
             if (!isset($subtotal)){
                 $price = number_format($product->getPrice(), 2);
-                $orderDAO->InsertOrder($price, $metodo, $dir, $date);
+                if ($metodo == 'Tarjeta Credito'){
+                    $orderDAO->InsertOrderCard($price, $metodo, $dir, $date);
+                } else {
+                    $orderDAO->InsertOrder($price, $metodo, $dir, $date);
+                }
                 // Obtener el ID del order recién insertado
                 $order_id = $orderDAO->getLastInsertID();
                 // Insertar en otra tabla utilizando el ID del order
@@ -159,13 +223,18 @@ ob_start();
             }
             else{
                 $price = $subtotal;
-                $orderDAO->InsertOrderCart($price, $metodo, $dir, $date, $cartCount);
+                if ($metodo == 'Tarjeta Credito'){
+                    $orderDAO->InsertOrderCartCard($price, $metodo, $dir, $date, $cartCount);
+                } else {
+                    $orderDAO->InsertOrderCart($price, $metodo, $dir, $date, $cartCount);
+                }
                 $order_id = $orderDAO->getLastInsertID();
                 $userOrderDAO->insert($userID, $order_id);
                 $userDAO->UpdateContact($userID, $nombre, $apellido, $new_email);
                 $userProductDAO->deleteCart();
             }
-            header('Location: orders.php');
+
+            header('Location: pay.php?metodo='. $metodo);
             exit();
         }
         ?>
@@ -178,3 +247,10 @@ ob_start();
 $content = ob_get_clean();
 require_once INCLUDES_ROOT . '/templates/default_template.php';
 ?>
+
+<style>
+    #buy-now {
+        font-size: 24px;
+        padding: 12px 24px;
+    }
+</style>
